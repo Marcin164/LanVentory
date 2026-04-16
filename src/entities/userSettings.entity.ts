48626ed29852f36b729bfe0bebcccc3ae549e86a
Marcin Nowakowski <1999nowakowskimarcin@gmail.com>
2026-04-16 14:21:24 +0200

    feat: add columns settings

--- src/entities/userSettings.entity.ts
+++ src/entities/userSettings.entity.ts
@@ -35,4 +35,32 @@ export class UserSettings {
 
   @Column({ default: 'small' })
   reportsLayout: string;
+
+  @Column({
+    type: 'jsonb',
+    default: [
+      { maxDays: 7, color: '#30A712', label: 'Recent' },
+      { maxDays: 30, color: '#F1C40F', label: 'Warning' },
+      { maxDays: 90, color: '#F3606E', label: 'Inactive' },
+    ],
+  })
+  lastLogonThresholds: Array<{
+    maxDays: number;
+    color: string;
+    label: string;
+  }>;
+
+  @Column({ default: '#8A8A8A' })
+  lastLogonDefaultColor: string;
+
+  @Column({ type: 'jsonb', default: {} })
+  filterPresets: Record<
+    string,
+    Array<{
+      id: string;
+      name: string;
+      filters: Record<string, any>;
+      lastUsed?: boolean;
+    }>
+  >;
 }
